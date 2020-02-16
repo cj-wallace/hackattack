527707d08326a3e98b945dae679b047843c5ae05
Connor Wallace <47456687+cj-wallace@users.noreply.github.com>
2020-02-16 17:14:39 -0500

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,28 @@ PORT = 6677  # Open Port, must match the client port
 score = 0
 
 
+def generate_reply(data):
+    reply = ''
+    # process your message
+    if data == 'hello':
+        reply = 'Hi, back!'
+    elif data == 'do-task':
+        reply = 'OK, I have done the task you have asked me!'
+    elif data == 'up':
+        global score
+        score += 1
+        reply = 'score up: ' + str(score)
+    elif data == 'down':
+        global score
+        score -= 1
+        reply = 'score down: ' + str(score)
+    else:
+        reply = 'Unknown command'
+
+    reply = reply.encode()
+    return reply
+
+
 def handle_thread(conn, addr):
     # Waiting for message
     while True:
@@ -14,27 +36,11 @@ def handle_thread(conn, addr):
             data = conn.recv(BUFF).decode()
             print addr[1], 'Received: %s' % data
 
-            reply = ''
-            # process your message
-            if data == 'hello':
-                reply = 'Hi, back!'
-            elif data == 'do-task':
-                reply = 'OK, I have done the task you have asked me!'
-            elif data == 'quit':
+            if data == 'quit':
                 conn.send('Terminate')
                 break
-            elif data == 'up':
-                global score
-                score += 1
-                reply = 'score up: ' + str(score)
-            elif data == 'down':
-                global score
-                score -= 1
-                reply = 'score down: ' + str(score)
-            else:
-                reply = 'Unknown command'
 
-            reply = reply.encode()
+            reply = generate_reply(data)
 
             conn.send(reply)  # Send reply
         except Exception as e:
@@ -66,3 +72,5 @@ while True:
     thread.start_new_thread(handle_thread, (conn, addr))
 s.close()
 print 'Socket Closed'
+
+
